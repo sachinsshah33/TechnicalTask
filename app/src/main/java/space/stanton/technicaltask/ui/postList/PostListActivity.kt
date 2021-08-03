fad7f77b94e76e4e7c0c8da22c99805fc33b95fb
Sachin Shah <sachin@nextbase.co.uk>
2021-08-03 16:09:54 +0100

    added offline badge count

--- app/src/main/java/space/stanton/technicaltask/ui/postList/PostListActivity.kt
+++ app/src/main/java/space/stanton/technicaltask/ui/postList/PostListActivity.kt
@@ -1,9 +1,11 @@
 package space.stanton.technicaltask.ui.postList
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
+import space.stanton.technicaltask.data.models.PostsUI
 import space.stanton.technicaltask.databinding.ActivityPostListBinding
 
 
@@ -16,6 +18,8 @@ class PostListActivity : AppCompatActivity() {
         ActivityPostListBinding.inflate(layoutInflater)
     }
 
+    private val postListViewModel: PostListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,6 +30,32 @@ class PostListActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
             tab.text = if (position == 0) "LIVE" else "OFFLINE"
         }.attach()
+
+        setupCachesPostCount()
+    }
+
+    private fun setupCachesPostCount() {
+        postListViewModel.cachedPostsUI.observe(this, {
+            when (it) {
+                is PostsUI.PostsSuccess -> {
+                    val count = it.items.size
+                    binding.tabLayout.getTabAt(1)?.let {
+                        if(count>0){
+                            val badge = it.orCreateBadge
+                            badge.number = count
+                        }
+                        else{
+                            it.removeBadge()
+                        }
+                    }
+                }
+                else -> {
+                    binding.tabLayout.getTabAt(1)?.removeBadge()
+                }
+            }
+        })
+
+        postListViewModel.getCachedPosts()
     }
 }
 
