7ad7b822f26478494b25d2d770a64b06ef6c9128
Sachin Shah <sachin@nextbase.co.uk>
2021-08-03 13:05:48 +0100

    binding cleanup

--- app/src/main/java/space/stanton/technicaltask/PostDetailActivity.kt
+++ app/src/main/java/space/stanton/technicaltask/PostDetailActivity.kt
@@ -5,15 +5,25 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import space.stanton.technicaltest.R
+import space.stanton.technicaltest.databinding.ActivityPostDetailsBinding
+import space.stanton.technicaltest.databinding.ActivityPostListBinding
 
 /**
  * Shows details of a post
  */
 class PostDetailActivity : AppCompatActivity() {
+    companion object {
+        const val POST_ID_KEY = "POST_ID_KEY"
+    }
+
+    val binding by lazy {
+        ActivityPostDetailsBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_details)
-        val id = intent.getStringExtra("postId")
+        setContentView(binding.root)
+        val id = intent.getStringExtra(POST_ID_KEY)
 
         Thread {
             ApiCalls.getPostById(id!!) {
@@ -22,12 +32,11 @@ class PostDetailActivity : AppCompatActivity() {
                 } else {
                     val post = JSONObject(it.first!!.string())
                     runOnUiThread {
-
-                        findViewById<TextView>(R.id.title).text = post.getString("title")
-                        findViewById<TextView>(R.id.content).text = post.getString("body")
-
-                        this@PostDetailActivity.title = post.getString("title")
-
+                        post.getString("title").let {
+                            binding.title.text = it
+                            this@PostDetailActivity.title = it
+                        }
+                        binding.content.text = post.getString("body")
                     }
                 }
             }
--- app/src/main/java/space/stanton/technicaltask/PostListActivity.kt
+++ app/src/main/java/space/stanton/technicaltask/PostListActivity.kt
@@ -3,7 +3,6 @@ package space.stanton.technicaltask
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,26 +10,33 @@ import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONArray
 import org.json.JSONObject
 import space.stanton.technicaltest.R
+import space.stanton.technicaltest.databinding.ActivityPostListBinding
+import space.stanton.technicaltest.databinding.ItemPostBinding
 
-class PostAdapter(val items: MutableList<JSONObject>, val onItemClick: (String) -> Unit) :
-    RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+class PostAdapter(val items: MutableList<JSONObject>, val onItemClick: (String) -> Unit) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 
-    class PostViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview)
+    inner class PostViewHolder(private val itemBinding: ItemPostBinding) : RecyclerView.ViewHolder(itemBinding.root){
+        fun bind(data: JSONObject) {
+            itemBinding.title.text = data.getString("title")
+            itemBinding.content.text = data.getString("body")
+            itemBinding.root.setOnClickListener {
+                onItemClick(data.getString("id"))
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         return PostViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_post, parent, false)
+            ItemPostBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         )
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        holder.itemView.findViewById<TextView>(R.id.title).text = items[position].getString("title")
-        holder.itemView.findViewById<TextView>(R.id.content).text =
-            items[position].getString("body")
-        holder.itemView.setOnClickListener {
-            onItemClick(items[position].getString("id"))
-        }
+        holder.bind(items[position])
     }
 
     override fun getItemCount(): Int = items.size
@@ -41,11 +47,13 @@ class PostAdapter(val items: MutableList<JSONObject>, val onItemClick: (String) 
  * Displays a list of posts
  */
 class PostListActivity : AppCompatActivity() {
-
+    val binding by lazy {
+        ActivityPostListBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         Thread {
             ApiCalls.loadAll {
@@ -58,11 +66,11 @@ class PostListActivity : AppCompatActivity() {
                         for (i in 0 until json.length()) {
                             listOfPosts.add(i, json.getJSONObject(i))
                         }
-                        findViewById<RecyclerView>(R.id.postsList).adapter =
+                        binding.postsList.adapter =
                             PostAdapter(listOfPosts, onItemClick = { id ->
                                 startActivity(
                                     Intent(this, PostDetailActivity::class.java)
-                                        .putExtra("postId", id)
+                                        .putExtra(PostDetailActivity.POST_ID_KEY, id)
                                 )
                             })
                     }
